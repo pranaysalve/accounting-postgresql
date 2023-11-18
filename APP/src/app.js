210f0b1b5ca1529332e86c1517e3d637ee019cce
Pranay Salve <64769068+pranaysalve@users.noreply.github.com>
2023-11-19 03:23:28 +0530

    updates

--- APP/src/app.js
+++ APP/src/app.js
@@ -3,15 +3,20 @@ const express = require("express");
 //Routers
 const Account = require("./route/Account.route");
 const AccountType = require("./route/AccountType.route");
-const CreditNote = require("./route/Account.route");
-const DebitNote = require("./route/Account.route");
-const DeliveryChallan = require("./route/Account.route");
-const Invoice = require("./route/Account.route");
-const InvoiceDetails = require("./route/Account.route");
-const PurchaseOrder = require("./route/Account.route");
-const PurchaseOrderDetails = require("./route/Account.route");
-const Vendor = require("./route/Vendor.route");
 const Bill = require("./route/Bill.route");
+const BillLineItem = require("./route/BillLineItem.route");
+const ChartOfAccount = require("./route/ChartOfAccounts");
+const Currency = require("./route/Currency.route");
+const Customer = require("./route/Customer.route");
+const FiscalYear = require("./route/FiscalYear.route");
+const Invoice = require("./route/Invoice.route");
+const InvoiceLineItem = require("./route/InvoiceLineItem.route");
+const PaymentMethod = require("./route/PaymentMethod.route");
+const Tax = require("./route/Tax.route");
+const Transaction = require("./route/Transaction.route");
+const TransactionEntry = require("./route/TransactionEntry.route");
+const User = require("./route/User.route");
+const Vendor = require("./route/Vendor.route");
 const app = express();
 
 app.use(express.json());
@@ -29,16 +34,21 @@ app.use((req, res, next) => {
 // final routes
 app.use("/api/account", Account);
 app.use("/api/accounttype", AccountType);
-app.use("/api/vendor", Vendor);
 app.use("/api/bill", Bill);
+app.use("/api/bill-details", BillLineItem);
+app.use("/api/chartofaccounts", ChartOfAccount);
+app.use("/api/currency", Currency);
+app.use("/api/customer", Customer);
+app.use("/api/fiscal-year", FiscalYear);
+app.use("/api/invoice", Invoice);
+app.use("/api/invoice-details", InvoiceLineItem);
+app.use("/api/payment-method", PaymentMethod);
+app.use("/api/tax", Tax);
+app.use("/api/transaction", Transaction);
+app.use("/api/transaction-entry", TransactionEntry);
+app.use("/api/user", User);
+app.use("/api/vendor", Vendor);
 
 // temp routes
-app.use("/api/invoice", Invoice);
-app.use("/api/invoice-details", InvoiceDetails);
-app.use("/api/credit-note", CreditNote);
-app.use("/api/debit-note", DebitNote);
-app.use("/api/delivery-challan", DeliveryChallan);
-app.use("/api/purchase-order", PurchaseOrder);
-app.use("/api/purchase-order-details", PurchaseOrderDetails);
 
 module.exports = app;
